0431eeb69e596b54aea26c1c27168240d63a43a2
Filmon <filmon@Lydias-Air.attlocal.net>
2022-06-21 15:24:24 -0400

    AuthContext. created.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import 'simplebar/dist/simplebar.min.css'
+
+import { ThemeProvider } from '@mui/material/styles'
+import lightTheme from './themes/lightTheme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <ThemeProvider theme={lightTheme}>
+          <App />
+      </ThemeProvider>
+  </React.StrictMode>
+);
